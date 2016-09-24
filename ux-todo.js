7354959b01ecd3f8052315f3c0e1f490215d86ba
Ivan Wills <Ivan.Wills2@optus.com.au>
2016-09-24 10:32:07 +1000

    Added some default data

--- ux-todo.js
+++ ux-todo.js
@@ -1,7 +1,17 @@
 import ractive from 'ractive';
-import template from './ux-todo.hbs';
+import template from './ux-todo.hbs.js';
 
 export default ractive.extend({
     template: template,
-    isolated: true;
+    isolated: true,
+    data: () => {
+        return {
+            todos: [
+                {
+                    name : 'Milk',
+                    value: 'Get the milk'
+                }
+            ]
+        };
+    }
 });
